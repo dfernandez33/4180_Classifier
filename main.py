026b7fcd790058ea83411962ff263af17b930b9b
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-11-21 00:08:38 -0500

    update main

--- main.py
+++ main.py
@@ -6,31 +6,30 @@ from joblib import dump, load
 import picamera
 
 
-def main():
-    print("Starting camera")
-    camera = picamera.PiCamera()
+print("Starting camera")
+camera = picamera.PiCamera()
 
-    print("starting model training")
+print("starting model training")
 
-    try:
-        img_clf = load('model.joblib')
-    except Exception as e:
-        img_clf = ImageClassifier()
-        # load images
-        (train_raw, train_labels) = img_clf.load_data_from_folder('./train/')
-        # convert images into features
-        train_data = img_clf.extract_image_features(train_raw)
-        # train model and test on training data
-        img_clf.train_classifier(train_data, train_labels)
-        # dump classifier into file for later use
-        dump(img_clf, 'model.joblib')
+try:
+    img_clf = load('model.joblib')
+except Exception as e:
+    img_clf = ImageClassifier()
+    # load images
+    (train_raw, train_labels) = img_clf.load_data_from_folder('./train/')
+    # convert images into features
+    train_data = img_clf.extract_image_features(train_raw)
+    # train model and test on training data
+    img_clf.train_classifier(train_data, train_labels)
+    # dump classifier into file for later use
+    dump(img_clf, 'model.joblib')
 
-    print("Model Trained")
+print("Model Trained")
 
-    while True:
-        stream = io.BytesIO()
-        time.sleep(2)
-        camera.capture(stream, format='jpeg')
-        # Construct a numpy array from the stream
-        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-        print(img_clf.predict_labels(img_clf.extract_image_features(data)))
+while True:
+    stream = io.BytesIO()
+    time.sleep(2)
+    camera.capture(stream, format='jpeg')
+    # Construct a numpy array from the stream
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    print(img_clf.predict_labels(img_clf.extract_image_features(data)))
