201d7691d4932196f8341f8c79023f284720e475
David Jose Fernandez <dfernandez33@gatech.edu>
2019-11-20 22:17:37 -0500

    commiting images

--- collectImages.py
+++ collectImages.py
@@ -3,25 +3,21 @@ import time
 import picamera
 
 
-def main():
-    myargs = sys.argv[1:]
 
-    camera = picamera.PiCamera()
+myargs = sys.argv[1:]
 
-    if len(myargs) <= 1:
-        sys.exit("Incorrect arguments")
+camera = picamera.PiCamera()
 
-    num_images_per_type = int(myargs[0])  # number of images to take of each type of object
+if len(myargs) <= 1:
+    sys.exit("Incorrect arguments")
 
-    print("Taking ", num_images_per_type, "images each of ", myargs[1:])
+num_images_per_type = int(myargs[0])  # number of images to take of each type of object
 
-    for image_type in myargs[1:]:
-        for i in range(num_images_per_type):
-            time.sleep(.5)
-            camera.start_preview()
-            time.sleep(1)
-            filename = image_type + "_" + str(i) + ".bmp"
-            camera.capture(filename, format='bmp')
-            camera.stop_preview()
-            print(image_type + str(i))
-            time.sleep(2)
+print("Taking ", num_images_per_type, "images each of ", myargs[1:])
+
+for image_type in myargs[1:]:
+    for i in range(num_images_per_type):
+        filename = image_type + "_" + str(i) + ".bmp"
+        camera.capture("images/"+filename, format='bmp')
+        print(filename)
+        time.sleep(1)
