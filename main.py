d912a8a212b7721168997f49d374e24957908b4d
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-11-21 00:09:45 -0500

    update main

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ while True:
     time.sleep(2)
     camera.capture(stream, format='jpeg')
     # Construct a numpy array from the stream
-    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
     print(img_clf.predict_labels(img_clf.extract_image_features(data)))
