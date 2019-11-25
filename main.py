40f3331f97992482d99a6570eee0ae9ff0519c2d
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-11-25 16:18:06 -0500

    updated main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ print("Model Trained")
 while True:
     stream = io.BytesIO()
     time.sleep(2)
-    output = np.empty((240, 320, 3), dtype=np.uint8)
+    output = np.empty((320, 240, 3), dtype=np.uint8)
     camera.capture(output, format='bmp', resize=(320, 240))
     # Construct a numpy array from the stream
     features = img_clf.extract_image_features(output)
