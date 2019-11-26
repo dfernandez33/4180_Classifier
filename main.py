25ff08a21e79d5c91d6b6ec979d5f12bbb4a8273
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-11-25 19:33:28 -0500

    updated main

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import numpy as np
 from PIL import Image
 from joblib import dump, load
 import picamera
+import serial
+
 
 
 print("Starting camera")
 camera = picamera.PiCamera()
 camera.resolution = (352, 240)
 camera.color_effects = (128, 128)  # turn camera to black and white
+
+mbed = serial.Serial('/dev/ttyACM0', baudrate=9600)
+time.sleep(1)
 
 print("starting model training")
 
@@ -31,9 +36,15 @@ print("Model Trained")
 
 while True:
     stream = io.BytesIO()
-    camera.capture(stream, format="rgb", resize=(352, 240))
+    camera.capture(stream, format="bmp", resize=(352, 240))
     image = Image.open(stream)
     data = np.array(image)
     # Construct a numpy array from the stream
     features = img_clf.extract_image_features([data])
-    print(img_clf.predict_labels(features)[0])
+    label = img_clf.predict_labels(features)[0]
+    if label == "disco":
+        mbed.write("d")
+    elif label == "tornado":
+        mbed.write("t")
+
+
